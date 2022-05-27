ea35c4ef68159b1e09f5b5d66b6941efc1ee68e9
jahnavi1126 <2000031423@kluniversity.in>
2022-05-27 18:47:11 +0530

    janu

--- engage/settings.py
+++ engage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,7 +75,7 @@ WSGI_APPLICATION = 'engage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
 
     'default': {
 
@@ -82,9 +83,9 @@ DATABASES = {
 
         'NAME': 'enagedb',
 
-        'USER': 'nvsai',
+        'USER': 'janu',
 
-        'PASSWORD': 'Nvsai222',
+        'PASSWORD': '1128',
 
         'HOST': '127.0.0.1',
 
@@ -93,7 +94,27 @@ DATABASES = {
     }
 
 }
+"""
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'mydb',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'engage@123',
+
+        'HOST': 'microsoftengage.postgres.database.azure.com',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -129,7 +150,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+   ('microsoft', os.path.join(BASE_DIR, 'microsoft', 'static')),
+)
+STATICFILES_FINDERS = (
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
